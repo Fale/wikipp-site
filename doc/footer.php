02265eeb29cb7b503c8d700e19739f6ef2d68a08
Fabio A Locati <fabiolocati@gmail.com>
2009-04-27 21:26:00 +0200

    doc/trunk way improved :)

---
+++ doc/footer.php
@@ -0,0 +1,20 @@
+        </div><!--content-->
+      </div><!--column-content-->
+      <div id="column-one">
+        <div id="p-logo" class="portlet">
+          <a href="http://wikipp.net/" style="background-image: url(../../images/png/ts.png);"></a>
+        </div><!--p-logo-->
+        <?php navigation(); ?>
+      </div><!--column-one-->
+      <div class="visualClear" />
+      <div id="footer">
+        <a href="http://validator.w3.org/check?uri=referer">
+          <img src="http://www.w3.org/Icons/valid-xhtml10-blue" alt="Valid XHTML 1.0 Transitional" height="31" width="88" />
+        </a>
+        <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/">
+          <img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/3.0/88x31.png" />
+        </a>
+      </div><!--footer-->
+    </div><!--globalWrapper-->
+  </body>
+</html>
