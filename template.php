8f67923eb556e40cc85c45ebc921f92110b0cbfe
Fabio Alessandro Locati <flocati@grimp.eu>
2011-01-13 13:17:47 +0100

    Add the sponsor logo

--- template.php
+++ template.php
@@ -60,6 +60,9 @@ function footer()
         <a href="http://validator.w3.org/check?uri=referer">
           <img src="http://www.w3.org/Icons/valid-xhtml10-blue" alt="Valid XHTML 1.0 Transitional" height="31" width="88" />
         </a>
+	<a href="http://grimp.eu">
+	  <img src="http://repo.grimp.eu/grimp/logo.png" height="31" />
+	</a>
         <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/">
           <img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/3.0/88x31.png" />
         </a>
