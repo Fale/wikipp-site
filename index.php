33d12bed47f0220f6768ce949ca4ab9fb28601e7
Fabio Alessandro Locati <flocati@grimp.eu>
2011-01-13 12:42:21 +0100

    We have new git repos :)

--- index.php
+++ index.php
@@ -6,8 +6,9 @@ headers("Wiki++ - Home Page");
 <h1>Wiki++</h1><br/>
 <p>Wiki++ is a framework written in C++ for C++ that has the goal of simplify the writing of C++ bots for wikis.</p>
 <p>Wiki++ is released under the terms of the GPLv3.</p>
-<p>Wiki++'s source is available in <a href="http://github.com/Fale/Wikipp/">my GIT</a>.</p>
-<p>Wiki++'s documentation is available <a href="doc/">here</a>.</p>
+<p>Wiki++ source is available <a href="http://git.grimp.eu/?p=wikipp-framework.git;a=summary">here</a>.</p>
+<p>Wiki++ examples are available <a href="http://git.grimp.eu/?p=wikipp-framework.git;a=summary">here</a>.</p>
+<p>Wiki++ documentation is available <a href="doc/">here</a>.</p>
 
 <?php
 footer();
