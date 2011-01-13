f5abb5b1a1cbaaa6b142f3ed767186e4ba159871
Fabio Alessandro Locati <flocati@grimp.eu>
2011-01-13 13:20:34 +0100

    Add alt to grimp image

--- template.php
+++ template.php
@@ -61,7 +61,7 @@ function footer()
           <img src="http://www.w3.org/Icons/valid-xhtml10-blue" alt="Valid XHTML 1.0 Transitional" height="31" width="88" />
         </a>
 	<a href="http://grimp.eu">
-	  <img src="http://repo.grimp.eu/grimp/logo.png" height="31" />
+	  <img src="http://repo.grimp.eu/grimp/logo.png" height="31" / alt="Grimp">
 	</a>
         <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/">
           <img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/3.0/88x31.png" />
