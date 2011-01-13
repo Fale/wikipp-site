12c86d69e1d44235d21595cd47e4ee96532f9224
Fabio Alessandro Locati <flocati@grimp.eu>
2011-01-13 13:28:57 +0100

    Fix the footer for doc too

--- doc/footer.php
+++ doc/footer.php
@@ -11,6 +11,9 @@
         <a href="http://validator.w3.org/check?uri=referer">
           <img src="http://www.w3.org/Icons/valid-xhtml10-blue" alt="Valid XHTML 1.0 Transitional" height="31" width="88" />
         </a>
+	<a href="http://grimp.eu">
+	  <img src="http://repo.grimp.eu/grimp/logo.png" height="31" alt="Grimp" />
+	</a>
         <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/">
           <img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/3.0/88x31.png" />
         </a>
